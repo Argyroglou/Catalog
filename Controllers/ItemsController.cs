2b7f176f85acb98cd7f56a7d211f5e193411b778
Basilis Argyroglou <b.argyroglou@metronsa.eu>
2021-10-19 14:05:34 +0300

    add logger

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -6,6 +6,7 @@ using Catalog.Dtos;
 using Catalog.Models;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Catalog.Controllers
 {
@@ -14,9 +15,11 @@ namespace Catalog.Controllers
     public class ItemsController : ControllerBase
     {
         private readonly IItemsRepository repository;
-        public ItemsController(IItemsRepository repository)
+        private readonly ILogger<ItemsController> logger;
+        public ItemsController(IItemsRepository repository, ILogger<ItemsController> logger)
         {
             this.repository = repository;
+            this.logger = logger;
         }
 
         //Get /items
@@ -24,6 +27,7 @@ namespace Catalog.Controllers
         public async Task<IEnumerable<ItemDto>> GetItemsAsync()
         {
             var items = (await repository.GetItemsAsync()).Select(item => item.AsDto());
+            logger.LogInformation($"{DateTime.UtcNow.ToString("hh:mm:ss")}: Retrieved {items.Count()} items");
             return items;
         }
 
